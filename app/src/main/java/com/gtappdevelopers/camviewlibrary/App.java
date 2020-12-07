7ffee8cbfb77d1acfbc40c4421b701df1157b895
chaitanyamunje@gmail.com <Munje@1069>
2020-12-07 10:02:32 +0530

    UIL (Universal Image Loader) library implementation in Android.

---
+++ app/src/main/java/com/gtappdevelopers/camviewlibrary/App.java
@@ -0,0 +1,31 @@
+package com.gtappdevelopers.camviewlibrary;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+
+        super.onCreate();
+        initImageLoader(getApplicationContext());
+    }
+
+    public static void initImageLoader(Context context) {
+
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(context)
+                .threadPriority(Thread.NORM_PRIORITY - 2)
+                .denyCacheImageMultipleSizesInMemory()
+                .discCacheFileNameGenerator(new Md5FileNameGenerator())
+                .tasksProcessingOrder(QueueProcessingType.LIFO)
+                .build();
+
+        ImageLoader.getInstance().init(config);
+    }
+
+}
--- app/src/main/java/com/gtappdevelopers/camviewlibrary/MainActivity.java
+++ app/src/main/java/com/gtappdevelopers/camviewlibrary/MainActivity.java
@@ -1,126 +1,48 @@
 package com.gtappdevelopers.camviewlibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 
-import static android.Manifest.permission.CAMERA;
-import static android.Manifest.permission.VIBRATE;
 
+import android.widget.ImageView;
 
-import android.widget.TextView;
-import android.widget.Toast;
-
-import eu.livotov.labs.android.camview.ScannerLiveView;
-import eu.livotov.labs.android.camview.camera.CameraController;
-import eu.livotov.labs.android.camview.scanner.decoder.zxing.ZXDecoder;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
 public class MainActivity extends AppCompatActivity {
-    private ScannerLiveView camera;
-    private TextView scannedTV;
+
+   private ImageView img;
+    DisplayImageOptions options;
+     ImageLoader imageLoader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // check permission method is to check that the camera permission is granted by user or not.
-        // request permission method is to request the camera permission if not given.
+        // initialize image loader before using
+        imageLoader= ImageLoader.getInstance();
+        imageLoader.init(ImageLoaderConfiguration.createDefault(getApplicationContext()));
 
-        if (checkPermission()){
-            // if permission is already granted display a toast message
-            Toast.makeText(this, "Permission Granted..", Toast.LENGTH_SHORT).show();
-        }
-        else {
-            requestPermission();
-        }
-        //initialize scannerLiveview and textview.
-        scannedTV = findViewById(R.id.idTVscanned);
-        camera = (ScannerLiveView) findViewById(R.id.camview);
-
-        camera.setScannerViewEventListener(new ScannerLiveView.ScannerViewEventListener() {
-            @Override
-            public void onScannerStarted(ScannerLiveView scanner) {
-                //method is called when scanner is started
-                Toast.makeText(MainActivity.this, "Scanner Started", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onScannerStopped(ScannerLiveView scanner) {
-                // method is called when scanner is stoped.
-                Toast.makeText(MainActivity.this, "Scanner Stopped", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onScannerError(Throwable err) {
-                // method is called when scanner gives some error.
-                Toast.makeText(MainActivity.this, "Scanner Error: " + err.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onCodeScanned(String data) {
-                // method is called when camera scans the qr code and the data from qr code is stored in data in string format.
-                scannedTV.setText(data);
-            }
-        });
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        ZXDecoder decoder = new ZXDecoder();
-        //0.5 is the area where we have to place red marker for scanning.
-        decoder.setScanAreaPercent(0.8);
-        //below method will set secoder to camera.
-        camera.setDecoder(decoder);
-        camera.startScanner();
-    }
-
-    @Override
-    protected void onPause() {
-        // on app pause the camera will stop scanning.
-        camera.stopScanner();
-        super.onPause();
-    }
-
-
-    private boolean checkPermission() {
-        // here we are checking two permission that is vibrate and camera which is granted by user and not.
-        // if permission is granted then we are returning true otherwise false.
-        int camera_permission = ContextCompat.checkSelfPermission(getApplicationContext(), CAMERA);
-        int vibrate_permission = ContextCompat.checkSelfPermission(getApplicationContext(), VIBRATE);
-
-        return camera_permission == PackageManager.PERMISSION_GRANTED && vibrate_permission == PackageManager.PERMISSION_GRANTED;
-    }
-
-
-    private void requestPermission() {
-        //this method is to request the runtime permission.
-        int PERMISSION_REQUEST_CODE = 200;
-        ActivityCompat.requestPermissions(this, new String[]{CAMERA, VIBRATE}, PERMISSION_REQUEST_CODE);
-
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        //this method is called when user allows the permission to use camera.
-        if (grantResults.length > 0) {
-
-            boolean cameraaccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
-            boolean vibrateaccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
-
-            if (cameraaccepted
-                    && vibrateaccepted) {
-                Toast.makeText(this, "Permission granted..", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(this, "Permission Denined \n You cannot use app without providing permssion"
-                        , Toast.LENGTH_SHORT).show();
-
-            }
-        }
+        //initialize imageview from activity_main.xml
+        img=findViewById(R.id.idImageView);
+         // URL for our image that we have to load..
+        String imageUri = "https://www.geeksforgeeks.org/wp-content/uploads/gfg_200X200-1.png";
+        //with below method we are setting display option for our image..
+        options = new DisplayImageOptions.Builder()
+                // stub image will display when your image is loading
+                .showStubImage(R.drawable.ic_launcher_foreground)
+                // below image will be displayed when the image url is empty
+                .showImageForEmptyUri(R.drawable.ic_launcher_background)
+                // cachememory method will caches the image in users external storage
+                .cacheInMemory()
+                // cache on disc will caches the image in users internal storage
+                .cacheOnDisc()
+                //. build will build the view for displaying image..
+                .build();
+        // below method will display image inside our image view..
+        imageLoader.displayImage(imageUri, img, options, null);
 
 
     }
