38296d6ebeee9a064727ae466ce98c0797d0e4e9
chaitanyamunje@gmail.com <Munje@1069>
2020-12-08 22:54:05 +0530

    Audio Recorder Implementation Example in Android.

--- app/src/main/java/com/gtappdevelopers/camviewlibrary/MainActivity.java
+++ app/src/main/java/com/gtappdevelopers/camviewlibrary/MainActivity.java
@@ -1,49 +1,197 @@
 package com.gtappdevelopers.camviewlibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 
 
-import android.widget.ImageView;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.nostra13.universalimageloader.core.DisplayImageOptions;
-import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import java.io.IOException;
+
+import static android.Manifest.permission.RECORD_AUDIO;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 public class MainActivity extends AppCompatActivity {
 
-   private ImageView img;
-    DisplayImageOptions options;
-     ImageLoader imageLoader;
+    //Intializing all variables..
+    private TextView startTV, stopTV, playTV, stopplayTV, statusTV;
+    //creating a variable for medi recorder object class.
+    private MediaRecorder mRecorder;
+    // creating a variable for mediaplayer class
+    private MediaPlayer mPlayer;
+    //string variable is created for storing a file name
+    private static String mFileName = null;
+    // constant for storing audio permission
+    public static final int REQUEST_AUDIO_PERMISSION_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // initialize image loader before using
-        imageLoader= ImageLoader.getInstance();
-        imageLoader.init(ImageLoaderConfiguration.createDefault(getApplicationContext()));
+        //initialize all variables with their layout items.
+        statusTV = findViewById(R.id.idTVstatus);
+        startTV = findViewById(R.id.btnRecord);
+        stopTV = findViewById(R.id.btnStop);
+        playTV = findViewById(R.id.btnPlay);
+        stopplayTV = findViewById(R.id.btnStopPlay);
+        stopTV.setBackgroundColor(getResources().getColor(R.color.gray));
+        playTV.setBackgroundColor(getResources().getColor(R.color.gray));
+        stopplayTV.setBackgroundColor(getResources().getColor(R.color.gray));
 
-        //initialize imageview from activity_main.xml
-        img=findViewById(R.id.idImageView);
-         // URL for our image that we have to load..
-        String imageUri = "https://www.geeksforgeeks.org/wp-content/uploads/gfg_200X200-1.png";
-        //with below method we are setting display option for our image..
-        options = new DisplayImageOptions.Builder()
-                // stub image will display when your image is loading
-                .showStubImage(R.drawable.ic_launcher_foreground)
-                // below image will be displayed when the image url is empty
-                .showImageForEmptyUri(R.drawable.ic_launcher_background)
-                // cachememory method will caches the image in users external storage
-                .cacheInMemory()
-                // cache on disc will caches the image in users internal storage
-                .cacheOnDisc()
-                //. build will build the view for displaying image..
-                .build();
-        // below method will display image inside our image view..
-        imageLoader.displayImage(imageUri, img, options, null);
+        startTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // start recording method will start the recording of audio.
+                startRecording();
+            }
+        });
+        stopTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //pause Recording method will pause the recording of audio.
+                pauseRecording();
+
+            }
+        });
+        playTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // play audio method will play the audio which we have recorded
+                playAudio();
+            }
+        });
+        stopplayTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // pause play method will pause the play of audio
+                pausePlaying();
+            }
+        });
+    }
+
+    private void startRecording() {
+        // check permission method is used to check that the user has granted permission to record nd store the audio.
+        if (CheckPermissions()) {
+            //setbackgroundcolor method will change the background color of text view.
+            stopTV.setBackgroundColor(getResources().getColor(R.color.purple_200));
+            startTV.setBackgroundColor(getResources().getColor(R.color.gray));
+            playTV.setBackgroundColor(getResources().getColor(R.color.gray));
+            stopplayTV.setBackgroundColor(getResources().getColor(R.color.gray));
+            //we are here initializing our filename variable with the path of the recorded audio file.
+            mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
+            mFileName += "/AudioRecording.3gp";
+            //below method is used to initialize the media recorder clss
+            mRecorder = new MediaRecorder();
+            //below method is used to set the audio source which we are using a mic.
+            mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            //below method is used to set the output format of the audio.
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            //below method is used to set the audio encoder for our recorded audio.
+            mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            //below method is used to set the output file location for our recorded audio
+            mRecorder.setOutputFile(mFileName);
+            try {
+                //below mwthod will prepare our audio recorder class
+                mRecorder.prepare();
+            } catch (IOException e) {
+                Log.e("TAG", "prepare() failed");
+            }
+            // start method will start the audio recording.
+            mRecorder.start();
+            statusTV.setText("Recording Started");
+        } else {
+            //if audio recording permissions are not granted by user below method will ask for runtime permission for mic and storage.
+            RequestPermissions();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        // this method is called when user will grant the permission for audio recording.
+        switch (requestCode) {
+            case REQUEST_AUDIO_PERMISSION_CODE:
+                if (grantResults.length > 0) {
+                    boolean permissionToRecord = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean permissionToStore = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                    if (permissionToRecord && permissionToStore) {
+                        Toast.makeText(getApplicationContext(), "Permission Granted", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Permission Denied", Toast.LENGTH_LONG).show();
+                    }
+                }
+                break;
+        }
+    }
+
+    public boolean CheckPermissions() {
+        //this method is used to check permission
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
+        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), RECORD_AUDIO);
+        return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void RequestPermissions() {
+        // this method is used to request the permission for audio recording and storage.
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{RECORD_AUDIO, WRITE_EXTERNAL_STORAGE}, REQUEST_AUDIO_PERMISSION_CODE);
+    }
+
+
+    public void playAudio() {
+        stopTV.setBackgroundColor(getResources().getColor(R.color.gray));
+        startTV.setBackgroundColor(getResources().getColor(R.color.purple_200));
+        playTV.setBackgroundColor(getResources().getColor(R.color.gray));
+        stopplayTV.setBackgroundColor(getResources().getColor(R.color.purple_200));
+        //for playing our recorded audio we are using media player class.
+        mPlayer = new MediaPlayer();
+        try {
+            //below method is used to set the data source which will be our file name
+            mPlayer.setDataSource(mFileName);
+            //below method will prepare our media player
+            mPlayer.prepare();
+            //below method will start our media player.
+            mPlayer.start();
+            statusTV.setText("Recording Started Playing");
+        } catch (IOException e) {
+            Log.e("TAG", "prepare() failed");
+        }
 
 
     }
+
+    public void pauseRecording() {
+        stopTV.setBackgroundColor(getResources().getColor(R.color.gray));
+        startTV.setBackgroundColor(getResources().getColor(R.color.purple_200));
+        playTV.setBackgroundColor(getResources().getColor(R.color.purple_200));
+        stopplayTV.setBackgroundColor(getResources().getColor(R.color.purple_200));
+        //below method will stop the audio recording.
+        mRecorder.stop();
+        //below method will release the media recorder class.
+        mRecorder.release();
+        mRecorder = null;
+        statusTV.setText("Recording Stopped");
+
+    }
+
+    public void pausePlaying() {
+        //this method will release the media player class and pause the playing of our recorded audio.
+        mPlayer.release();
+        mPlayer = null;
+        stopTV.setBackgroundColor(getResources().getColor(R.color.gray));
+        startTV.setBackgroundColor(getResources().getColor(R.color.purple_200));
+        playTV.setBackgroundColor(getResources().getColor(R.color.purple_200));
+        stopplayTV.setBackgroundColor(getResources().getColor(R.color.gray));
+        statusTV.setText("Recording Play Stopped");
+
+    }
+
 }
